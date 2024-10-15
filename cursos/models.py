f19289547e3af38e8ac6761c42bb4fa9b401b239
MOISES-DARLAN <moisesdarlan440@gmail.com>
2024-10-15 17:40:59 -0300

    feat: criar a primeira migration

--- cursos/models.py
+++ cursos/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
-# Create your models here.
 class Curso(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200, )
+    title = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)  
+    professor = models.CharField(max_length=200, null=True, blank=True)  
+    data = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.title
