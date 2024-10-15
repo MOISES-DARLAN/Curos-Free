9143a6831acfdb59924f70434b66025c8786a714
MOISES-DARLAN <moisesdarlan440@gmail.com>
2024-10-15 18:01:22 -0300

    feat: criar stackadmin

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from cursos.models import course
+from cursos.models import StackCourse
 
-# Register your models here.
+
+class StackAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'link', 'professor', 'data', 'stack',)
+    search_fields = ('title', 'link', 'professor', 'data', 'stack',)
+
+
+admin.site.register(StackCourse, StackAdmin)
+admin.site.register(course, CoursesAdmin)
--- cursos/models.py
+++ cursos/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-class Curso(models.Model):
+
+class StackCourse(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class course(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
+    stack = models.ForeignKey(StackCourse, on_delete=models.PROTECT, related_name='stack_courses')
     link = models.CharField(max_length=200)  
     professor = models.CharField(max_length=200, null=True, blank=True)  
     data = models.DateField(auto_now=True)
