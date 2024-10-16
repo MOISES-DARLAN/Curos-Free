68f10330949e9e5ba49088686c5f60e8590152f8
MOISES-DARLAN <moisesdarlan440@gmail.com>
2024-10-16 07:21:37 -0300

    feat: fazer a primeira consulta dos dados e retornar para o user

--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cursos.views import course_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cursos/', course_view)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from cursos.models import Course
 # Create your views here.
+
+def course_view(request):
+    cursos = Course.objects.all()
+    print(cursos)
+    return render(request,
+                'couses.html',
+                 {'courses':  cursos}
+             )
