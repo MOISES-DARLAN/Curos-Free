fadf2c56db0f849b6f9bffa6658a46007e7c02f1
MOISES-DARLAN <moisesdarlan440@gmail.com>
2024-10-17 14:06:05 -0300

    fiat: criar tela de login

--- acounts/views.py
+++ acounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def register_view(request):
     if request.method == 'POST':
@@ -12,3 +12,10 @@ def register_view(request):
         user_form = UserCreationForm()
 
     return render(request, 'register.html', {'user_form': user_form})
+
+
+def login_view(request):
+    if request.method == 'POST' :
+        pass
+    login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
--- app/urls.py
+++ app/urls.py
@@ -20,11 +20,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cursos.views import courses_view, new_course_view
-from acounts.views import register_view
+from acounts.views import register_view, login_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cursos/', courses_view, name='cursos'),
     path('new_course/', new_course_view, name='new_course'),
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login_view')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
